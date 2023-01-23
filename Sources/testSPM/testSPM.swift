f4282f1713c7cd0b37ac64b05bf625173a1d6d1d
AJignesh <40273369+AJignesh@users.noreply.github.com>
2023-01-23 22:21:36 +0530

    Update testSPM.swift

--- Sources/testSPM/testSPM.swift
+++ Sources/testSPM/testSPM.swift
@@ -1,5 +1,5 @@
 public struct testSPM {
-    public private(set) var text = "This is initial project"
+    public private(set) var text = "This is v2023.1.1"
 
     public init() {
         print(text)
